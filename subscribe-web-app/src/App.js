b2529f30c1010093443a645ce9823793b6b2e9df
barnaby-smith <bs663@cam.ac.uk>
2024-03-16 21:45:38 +0000

    Started working on smart wallet creation

--- subscribe-web-app/src/App.js
+++ subscribe-web-app/src/App.js
@@ -3,8 +3,10 @@ import { NavBar, SubscriptionContent, Footer } from './my-react-components'
 import { WalletComponent } from './WalletConnection';
 import { Container } from 'react-bootstrap';
 import detectEthereumProvider from '@metamask/detect-provider';
+import { MetaMaskWallet, SmartWallet } from "@thirdweb-dev/wallets";
+import { Sepolia } from "@thirdweb-dev/chains"
 
-const provider = await detectEthereumProvider();
+// const provider = await detectEthereumProvider();
 
 function PageContent() {
   const [connected, setConnected] = useState(false)
@@ -22,38 +24,74 @@ function PageContent() {
     }
   }
 
-  if (provider) {
-    startApp(provider);
-  } else {
-    console.log('Please install MetaMask!');
-  }
-
-  function startApp(provider) {
-    if (provider !== window.ethereum) {
-      console.error('Do you have multiple wallets installed?');
-    }
-  }
-
-  async function getAccount() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-      .catch((err) => {
-        if (err.code === 4001) {
-          console.log('Please connect to MetaMask.');
-        } else {
-          console.error(err);
-        }
-      });
+  async function connectMetaMask() {
+    const personalWallet = new MetaMaskWallet({
+      clientId: "97c1d7a82eae7d2b4c4563c09874f7cf",
+      chains: [Sepolia],
+    });
     try {
-      const account = accounts[0];
-      setAccountAddress(account)
+      await personalWallet.connect()
+      const address = await personalWallet.getAddress()
+      console.log(address)
+      setAccountAddress(address)
       setWalletConnectErrorText("")
       setConnected(true)
-      checkSubscribed(account)
+      checkSubscribed(address)
+      const smartWallet = new SmartWallet(config)
+      console.log(smartWallet)
+      await smartWallet.connect({
+        personalWallet,
+      })
     } catch (err) {
       console.log(err)
       setWalletConnectErrorText("Please try again.")
     }
+    // console.log(wallet)
   }
+
+  const config = {
+    chain: Sepolia, // the chain where your smart wallet will be or is deployed
+    factoryAddress: "0xCb5894a8C4d5686AaFeAE5Dc0776DF1e6a9F2D8b", // your own deployed account factory address
+    clientId: "97c1d7a82eae7d2b4c4563c09874f7cf", // Use client id if using on the client side, get it from dashboard settings
+    // secretKey: "bA5Oe8xjqkOJGsufMxp8DpbW1yXyn3-CWTZsZluuJkcQAMIBxw6iAxIiRqM0ehqoTlSVWaRTbzWdYuUdKNQXyw", // Use secret key if using on the server, get it from dashboard settings
+    gasless: false, // enable or disable gasless transactions
+    chains: [Sepolia],
+  };
+
+
+
+  // if (provider) {
+  //   startApp(provider);
+  // } else {
+  //   console.log('Please install MetaMask!');
+  // }
+
+  // function startApp(provider) {
+  //   if (provider !== window.ethereum) {
+  //     console.error('Do you have multiple wallets installed?');
+  //   }
+  // }
+
+  // async function getAccount() {
+  //   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  //     .catch((err) => {
+  //       if (err.code === 4001) {
+  //         console.log('Please connect to MetaMask.');
+  //       } else {
+  //         console.error(err);
+  //       }
+  //     });
+  // try {
+  //   const account = accounts[0];
+  //   setAccountAddress(account)
+  //   setWalletConnectErrorText("")
+  //   setConnected(true)
+  //   checkSubscribed(account)
+  // } catch (err) {
+  //   console.log(err)
+  //   setWalletConnectErrorText("Please try again.")
+  // }
+  // }
 
   function subscribe() {
     subscribers[accountAddress] = true
@@ -65,7 +103,7 @@ function PageContent() {
     <>
       <NavBar subscribed={subscribed}></NavBar>
       <Container fluid className="d-flex flex-column align-items-center">
-        <WalletComponent connected={connected} getAccount={getAccount} errorText={walletConnectErrorText} accountAddress={accountAddress}></WalletComponent>
+        <WalletComponent connected={connected} getAccount={connectMetaMask} errorText={walletConnectErrorText} accountAddress={accountAddress}></WalletComponent>
         <SubscriptionContent connected={connected} subscribed={subscribed} subscribe={subscribe}></SubscriptionContent>
       </Container>
       <Footer></Footer>
